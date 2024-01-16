dd608619506c3e5c21530ea43bf6c837133b54c2
EdithRW <edith.saldana@rulesware.com>
2024-01-16 14:12:05 -0600

    change base url to 8000

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // The following environment variables are set by the build process. See the README for more information
   production: false,
-  base_url : 'http://localhost:3000/api'
+  base_url : 'http://localhost:8000/api'
 }
